113ba6e0a2ec0f673b4fba66e28fca0cb580f766
Tim Sears <tim@timsears.com>
2020-12-10 20:44:53 -0800

    use callCabal2nix

--- flake.nix
+++ flake.nix
@@ -1,25 +1,42 @@
 {
   description = "fizzbuzz haskell project demo";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
   inputs.nixpkgs.url = flake:unstable;
+  
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
+        
+        name = "fizzbuzz";
+        compiler = "ghc8102";
+        useHoogle = true;
+        
         pkgs = import nixpkgs {
           inherit system;
-          config = { allowUnfree = true;
-                     allowBroken = true;
-                     allowUnsupportedSystem = true;
-                   };
+          config = {
+            allowUnfree = true;
+            allowBroken = true;
+            allowUnsupportedSystem = true;
+          };
         };
+
+        hspkgs = pkgs.haskell.packages.${compiler}.extend (self: super: {
+          ghc = super.ghc //
+                { withPackages = if useHoogle then super.ghc.withHoogle else super.ghc.withPackages;
+                  ghcWithPackages = self.ghcWithPackages;
+                };
+          # other haskell overrides here...
+        });
+
+        drv = hspkgs.callCabal2nix name ./. {};
 
       in
         rec {
-          devShell = (import ./shell.nix { inherit pkgs; }).env;
-          defaultPackage = (import ./shell.nix { inherit pkgs; });
+          devShell = drv.env;
+          #devShell = (import ./shell.nix { inherit pkgs; }).env;
+          defaultPackage = drv;
         }
     );
 }
